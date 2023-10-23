a5cc07c2b6501b888214394e17f6d5b615185ac3
Willian Danko Caboski <caboskidanko@gmail.com>
2023-10-23 20:30:39 -0300

    Update baralho.c

--- Sakaue/pilha/baralho/baralho.c
+++ Sakaue/pilha/baralho/baralho.c
@@ -22,7 +22,8 @@ int main() {
         
     }
 
-    // desempilhar(&topo);
+    embaralhar(&topo);
+
     if(topo){
     printf("%d",topo->posicao);
     printf("%s", "\n ");
@@ -52,7 +53,7 @@ celula* empilhar(celula **topo, int n, int nipe) {
     novo->nipe = tipo[nipe - 1];
 
     novo->inferior = *topo;
-    *topo = novo; // Update the value of the pointer to the top of the stack
+    *topo = novo;
         
     return novo;
 }
@@ -68,4 +69,47 @@ int desempilhar(celula** topo) {
     int valor = excluir->valor;
     free(excluir); // Deallocate memory
     return valor;
+}
+
+void embaralhar(celula **topo) {
+    celula *nova_pilha = NULL;
+    int tamanho = 0;
+
+    // Calculate the size of the linked list
+    celula *atual = *topo;
+    while (atual != NULL) {
+        tamanho++;
+        atual = atual->inferior;
+    }
+
+    // Shuffle the linked list
+    for (int i = 0; i < tamanho; i++) {
+        // Generate a random number between 0 and tamanho - i - 1
+        int random_index = rand() % (tamanho - i);
+
+        // Find the node at the random index
+        atual = *topo;
+        for (int j = 0; j < random_index; j++) {
+            if(atual->posicao != 0){
+                atual = atual->inferior;
+            }
+            
+        }
+
+        // Remove the node from the original linked list
+        if (atual == *topo) {
+            *topo = atual->inferior;
+        } else {
+            celula *anterior = *topo;
+            while (anterior->inferior != atual) {
+                anterior = anterior->inferior;
+            }
+            anterior->inferior = atual->inferior;
+        }
+
+        // Add the node to the shuffled linked list
+        atual->inferior = nova_pilha;
+        nova_pilha = atual;
+        topo = &nova_pilha;
+    }
 }
